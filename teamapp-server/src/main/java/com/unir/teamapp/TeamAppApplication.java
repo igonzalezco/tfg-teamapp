3a72e5822ebeeee163cf67493c2c71546b3384a5
Ivan Gonzalez Cotelo <ivan.gonzalez368@comunidadunir.net>
2025-04-24 02:47:34 +0200

    Creación del proyecto base con todos los módulos. Se añaden en los poms las librerías que debería usar de base.

---
+++ teamapp-server/src/main/java/com/unir/teamapp/TeamAppApplication.java
@@ -0,0 +1,13 @@
+package com.unir.teamapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamAppApplication.class, args);
+    }
+    
+}
